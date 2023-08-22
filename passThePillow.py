7eecdc8f007fb738f4c7c8f898a69578ac0ac7e6
MatjažM <114862741+MatjazM2020@users.noreply.github.com>
2023-08-22 22:29:58 +0200

    Add files via upload

---
+++ passThePillow.py
@@ -0,0 +1,31 @@
+class Solution:
+    def passThePillow(self, n: int, time: int) -> int:
+       a = 1
+       tf = True
+       while time > 0:
+          if time == 0: 
+             return a
+          elif  a < n and tf : 
+             a+=1
+             time-=1
+             if a == n:
+                tf = False
+          elif  a>0 and not(tf): 
+             a-=1
+             time-=1
+             if a == 1:
+                tf = True
+       return a
+             
+
+       
+#https://leetcode.com/problems/pass-the-pillow/
+#There are n people standing in a line labeled from 1 to n. 
+# The first person in the line is holding a pillow initially.
+# Every second, the person holding the pillow passes it to the
+# next person standing in the line. Once the pillow reaches the
+# end of the line, the direction changes, and people continue passing the pillow
+# in the opposite direction.For example, once the pillow reaches the nth person they pass
+# it to the n - 1th person, then to the n - 2th person and so on. 
+# Given the two positive integers n and time, return the index of the person holding 
+# the pillow after time seconds.
