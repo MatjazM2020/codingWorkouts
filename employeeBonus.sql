ca8eacc083400c2036d37061be7e1f5a9e839007
MatjažM <114862741+MatjazM2020@users.noreply.github.com>
2023-09-24 16:11:33 +0200

    Add files via upload

---
+++ employeeBonus.sql
@@ -0,0 +1,39 @@
+select 
+name,
+bonus
+from 
+Employee
+left join 
+Bonus 
+on Employee.empId = Bonus.empId
+where coalesce(bonus,0) < 1000
+
+
+
+/*
+Exercise: https://leetcode.com/problems/employee-bonus/?envType=study-plan-v2&envId=top-sql-50
+#+-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| empId       | int     |
+| name        | varchar |
+| supervisor  | int     |
+| salary      | int     |
++-------------+---------+
+empId is the column with unique values for this table.
+Each row of this table indicates the name and the ID of an employee in addition to their salary and the id of their manager.
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| empId       | int  |
+| bonus       | int  |
++-------------+------+
+empId is the column of unique values for this table.
+empId is a foreign key (reference column) to empId from the Employee table.
+Each row of this table contains the id of an employee and their respective bonus.
+
+
+Write a solution to report the name and bonus amount of each employee with a bonus less than 1000.
+Return the result table in any order.
+The result format is in the following example.
+*/
