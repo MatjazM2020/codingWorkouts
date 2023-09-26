b4b93c9eae4d0d97bdbdffdb7d77b1cb6b41de47
MatjažM <114862741+MatjazM2020@users.noreply.github.com>
2023-09-26 16:44:33 +0200

    Update studentsAndExaminations.sql

--- studentsAndExaminations.sql
+++ studentsAndExaminations.sql
@@ -1,4 +1,19 @@
 select 
+x.student_id,
+student_name, 
+x.subject_name,
+coalesce(number,0) as attended_exams
+from
+(select
+student_id, 
+student_name, 
+subject_name 
+from 
+Students
+cross join 
+Subjects) as x
+left join 
+(select 
 student_id,
 subject_name, 
 count(subject_name) as number
@@ -7,24 +22,13 @@ Examinations
 group by 
 subject_name, 
 student_id
+) as y
+on x.student_id = y.student_id and 
+x.subject_name = y.subject_name 
+order by student_id, subject_name
 
-
-
-select
-student_id, 
-student_name, 
-subject_name 
-from 
-Students
-cross join 
-Subjects
-
-
-
-
-
-/* 
-Table: Students
+/*
+table: Students
 +---------------+---------+
 | Column Name   | Type    |
 +---------------+---------+
@@ -34,7 +38,8 @@ Table: Students
 student_id is the primary key (column with unique values) for this table.
 Each row of this table contains the ID and the name of one student in the school.
 
-Table: Subjects
+
+table: Subjects
 +--------------+---------+
 | Column Name  | Type    |
 +--------------+---------+
@@ -43,7 +48,7 @@ Table: Subjects
 subject_name is the primary key (column with unique values) for this table.
 Each row of this table contains the name of one subject in the school.
 
-Table: Examinations: 
+table: Examinations
 +--------------+---------+
 | Column Name  | Type    |
 +--------------+---------+
@@ -54,6 +59,8 @@ There is no primary key (column with unique values) for this table. It may conta
 Each student from the Students table takes every course from the Subjects table.
 Each row of this table indicates that a student with ID student_id attended the exam of subject_name.
 
+
 Write a solution to find the number of times each student attended each exam.
 Return the result table ordered by student_id and subject_name.
 The result format is in the following example.
+*/
