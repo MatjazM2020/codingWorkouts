76ca5c72970ca745ab21f2e75995c8eb4d6931d4
Matjaž Madon <114862741+MatjazM2020@users.noreply.github.com>
2024-01-07 21:52:39 +0100

    Add files via upload

---
+++ removeElement.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        k = 0 
+        for i in range(len(nums)): 
+            if nums[i] != val: 
+                nums[k] = nums[i] 
+                k += 1
+        return k
+    
+    
+    
+    
+#https://leetcode.com/problems/remove-element/
+#Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+# The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+#Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+#Change the array nums such that the first k elements of nums contain the elements which are not equal to val. 
+# The remaining elements of nums are not important as well as the size of nums. Return k.
