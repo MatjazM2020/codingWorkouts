39bec96c12738f68ad9fe8e760c98dc04759860f
Matjaž Madon <114862741+MatjazM2020@users.noreply.github.com>
2024-01-07 22:09:18 +0100

    Add files via upload

---
+++ addBinary.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        return bin(int(a,2)+int(b,2))[2:]
+
+
+
+
+
+#https://leetcode.com/problems/add-binary/description/
+#Given two binary strings a and b, return their sum as a binary string
