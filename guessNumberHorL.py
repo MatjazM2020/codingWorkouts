ec8aebe646e4d470f5a54727dd9557dbfa14badc
Matjaž Madon <114862741+MatjazM2020@users.noreply.github.com>
2024-08-08 13:49:13 +0200

    Add files via upload

---
+++ guessNumberHorL.py
@@ -0,0 +1,43 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        
+        l, r = 1, n
+        while l<=r:
+            c = (l+r)//2
+            g = guess(c)
+            if g == -1:
+                r = c-1
+            elif g == 1:
+                l = c+1
+            else:
+                return c
+    
+    
+'''
+https://leetcode.com/problems/guess-number-higher-or-lower/?envType=study-plan-v2&envId=binary-search
+
+We are playing the Guess Game. The game is as follows:
+I pick a number from 1 to n. You have to guess which number I picked.
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+
+'''
