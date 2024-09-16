6fbe2796a669cd6bff4dfa8bb174076d8b8680f9
MatjazM2020 <114862741+MatjazM2020@users.noreply.github.com>
2024-09-16 14:18:19 +0200

    Add files via upload

---
+++ findTheCenterOfStarGraph.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        return set(edges[0]).intersection(set(edges[1])).pop()
+    
+    
+'''
+https://leetcode.com/problems/find-center-of-star-graph/description/
+
+There is an undirected star graph consisting of n nodes labeled from 1 to n. A star graph is 
+a graph where there is one center node and exactly n - 1 edges that connect the center node with every other node.
+
+You are given a 2D integer array edges where each edges[i] = [ui, vi] indicates that there is an
+edge between the nodes ui and vi. Return the center of the given star graph.
+
+
+
+'''
