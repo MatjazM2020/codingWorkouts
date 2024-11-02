c369a30e1f1b1a42116c1a4b984eacfe6743851b
MatjazM2020 <114862741+MatjazM2020@users.noreply.github.com>
2024-11-02 10:21:40 +0100

    Add files via upload

--- leetflexBannedAccounts.sql
+++ leetflexBannedAccounts.sql
@@ -30,4 +30,36 @@ It is guaranteed that the logout time is after the login time.
 Write a solution to find the account_id of the accounts that should be banned from Leetflex. An account should be banned if it was logged in at some moment from two different IP addresses.
 
 Return the result table in any order.
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+LogInfo table:
++------------+------------+---------------------+---------------------+
+| account_id | ip_address | login               | logout              |
++------------+------------+---------------------+---------------------+
+| 1          | 1          | 2021-02-01 09:00:00 | 2021-02-01 09:30:00 |
+| 1          | 2          | 2021-02-01 08:00:00 | 2021-02-01 11:30:00 |
+| 2          | 6          | 2021-02-01 20:30:00 | 2021-02-01 22:00:00 |
+| 2          | 7          | 2021-02-02 20:30:00 | 2021-02-02 22:00:00 |
+| 3          | 9          | 2021-02-01 16:00:00 | 2021-02-01 16:59:59 |
+| 3          | 13         | 2021-02-01 17:00:00 | 2021-02-01 17:59:59 |
+| 4          | 10         | 2021-02-01 16:00:00 | 2021-02-01 17:00:00 |
+| 4          | 11         | 2021-02-01 17:00:00 | 2021-02-01 17:59:59 |
++------------+------------+---------------------+---------------------+
+Output: 
++------------+
+| account_id |
++------------+
+| 1          |
+| 4          |
++------------+
+Explanation: 
+Account ID 1 --> The account was active from "2021-02-01 09:00:00" to "2021-02-01 09:30:00" with two different IP addresses (1 and 2). It should be banned.
+Account ID 2 --> The account was active from two different addresses (6, 7) but in two different times.
+Account ID 3 --> The account was active from two different addresses (9, 13) on the same day but they do not intersect at any moment.
+Account ID 4 --> The account was active from "2021-02-01 17:00:00" to "2021-02-01 17:00:00" with two different IP addresses (10 and 11). It should be banned.
 */
