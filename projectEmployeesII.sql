6030a2d0a8c2cd276ba93fac34166761e2719a5e
MatjazM2020 <114862741+MatjazM2020@users.noreply.github.com>
2024-11-22 18:47:21 +0100

    Add files via upload

---
+++ projectEmployeesII.sql
@@ -0,0 +1,85 @@
+with Employee_counts as (
+    select 
+        project_id
+        , count(employee_id) num_of_employees
+    from Project
+    group by project_id)
+, max_num as (select 
+    max(num_of_employees)
+from
+Employee_counts)
+select 
+    project_id
+from Employee_counts
+where num_of_employees = (select * from max_num)
+
+
+
+/*
+https://leetcode.com/problems/project-employees-ii/description/
+
+Table: Project
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| project_id  | int     |
+| employee_id | int     |
++-------------+---------+
+(project_id, employee_id) is the primary key (combination of columns with unique values) of this table.
+employee_id is a foreign key (reference column) to Employee table.
+Each row of this table indicates that the employee with employee_id is working on the project with project_id.
+ 
+
+Table: Employee
+
++------------------+---------+
+| Column Name      | Type    |
++------------------+---------+
+| employee_id      | int     |
+| name             | varchar |
+| experience_years | int     |
++------------------+---------+
+employee_id is the primary key (column with unique values) of this table.
+Each row of this table contains information about one employee.
+ 
+
+Write a solution to report all the projects that have the most employees.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Project table:
++-------------+-------------+
+| project_id  | employee_id |
++-------------+-------------+
+| 1           | 1           |
+| 1           | 2           |
+| 1           | 3           |
+| 2           | 1           |
+| 2           | 4           |
++-------------+-------------+
+Employee table:
++-------------+--------+------------------+
+| employee_id | name   | experience_years |
++-------------+--------+------------------+
+| 1           | Khaled | 3                |
+| 2           | Ali    | 2                |
+| 3           | John   | 1                |
+| 4           | Doe    | 2                |
++-------------+--------+------------------+
+Output: 
++-------------+
+| project_id  |
++-------------+
+| 1           |
++-------------+
+Explanation: The first project has 3 employees while the second one has 2.
+
+*/
